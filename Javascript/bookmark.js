8f50ebca2953e73f773496dd7edcec9c116deb0b
Rafik Ali <79234312+rafikali@users.noreply.github.com>
2021-06-15 22:39:07 +0545

    Add files via upload

---
+++ Javascript/bookmark.js
@@ -0,0 +1,33 @@
+console.log("helloworld")
+
+let acc = document.getElementsByClassName("accordion");
+
+console.log(acc);
+ 
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+function openCity(evt, liName) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "block");
+  }
+  document.getElementById(liName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
